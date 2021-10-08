ec15865c2dfed4c7932493568ef2672e2c13ddc0
karyna <9548026git*>
2021-10-08 15:36:56 +0300

    added projects description

---
+++ src/utils/projects.js
@@ -0,0 +1,34 @@
+const ProjectItems = [
+    {
+        link: "https://github.com/Karinkaaa/portfolio",
+        title: "Portfolio",
+        subtitle: "Development of my portfolio site",
+        technologies: ["THREE.js", "JavaScript", "React", "Material UI", "HTML", "CSS"]
+    },
+    {
+        link: "https://github.com/Karinkaaa/spin-admin",
+        title: "Spin Admin",
+        subtitle: "Development of the admin part for the Spinner game",
+        technologies: ["JavaScript", "React", "Redux", "Redux-Saga", "Mirage.js", "Faker", "Material UI", "HTML", "CSS"]
+    },
+    {
+        link: "https://github.com/Karinkaaa/Products-front",
+        title: "Products",
+        subtitle: "Development of the site for creating, editing, deleting products",
+        technologies: ["JavaScript", "React", "Redux", "Redux-Saga", "Material UI", "HTML", "CSS"]
+    },
+    {
+        link: "https://github.com/Karinkaaa/Stamina",
+        title: "Stamina",
+        subtitle: "Online program for mastering ten-finger typing and touch typing",
+        technologies: ["JavaScript", "React", "Material UI", "HTML", "CSS"]
+    },
+    {
+        link: "https://github.com/Karinkaaa/WebBooks",
+        title: "WebBooks",
+        subtitle: "Development of the admin part for the Spinner game",
+        technologies: ["Java", "Spring", "Maven", "JSP", "HTML", "CSS"]
+    }
+];
+
+export default ProjectItems;
