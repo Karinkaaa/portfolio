d9587d06926bd59ffee718a2664533e9948f00d1
Karyna <kharinakaryna.com>
2023-03-23 10:52:38 +0200

    added new project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,40 +1,97 @@
 const ProjectItems = [
-     {
-        link: "https://karinkaaa.github.io/todo-list/",
-        title: "Todo List",
-        subtitle: "Web application for task management",
-        technologies: ["TypeScript", "React", "Redux", "Redux-Toolkit", "Material UI", "HTML", "CSS"]
-    },
-    {
-        link: "https://karinkaaa.github.io/News/",
-        title: "News",
-        subtitle: "Integration with an external server",
-        technologies: ["JavaScript", "React", "Redux", "Redux-Saga", "Material UI", "HTML", "CSS"]
-    },
-    {
-        link: "https://karinkaaa.github.io/portfolio/",
-        title: "Portfolio",
-        subtitle: "Development of my portfolio site",
-        technologies: ["THREE.js", "JavaScript", "React", "Material UI", "HTML", "CSS"]
-    },
-    {
-        link: "https://karinkaaa.github.io/Stamina/",
-        title: "Stamina",
-        subtitle: "Online program for mastering ten-finger typing and touch typing",
-        technologies: ["JavaScript", "React", "Material UI", "HTML", "CSS"]
-    },
-    {
-        link: "https://karinkaaa.github.io/spin-admin-deploy",
-        title: "Spin Admin",
-        subtitle: "Development of the admin part for the Spinner game",
-        technologies: ["JavaScript", "React", "Redux", "Redux-Saga", "Mirage.js", "Faker", "Material UI", "HTML", "CSS"]
-    },
-    {
-        link: "https://karinkaaa.github.io/Products-front/",
-        title: "Products",
-        subtitle: "Home project for learning JavaScript, React and Redux",
-        technologies: ["JavaScript", "React", "Redux", "Redux-Saga", "Material UI", "HTML", "CSS"]
-    },
+  {
+    link: "https://rick-and-morty-characters-web.netlify.app/",
+    title: "Rick And Morty Characters",
+    subtitle: "Rick and Morty character viewer web application",
+    technologies: [
+      "TypeScript",
+      "React",
+      "React Router",
+      "Redux",
+      "Redux-Toolkit",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "Google OAuth2",
+    ],
+  },
+  {
+    link: "https://karinkaaa.github.io/todo-list/",
+    title: "Todo List",
+    subtitle: "Web application for task management",
+    technologies: [
+      "TypeScript",
+      "React",
+      "Redux",
+      "Redux-Toolkit",
+      "Material UI",
+      "HTML",
+      "CSS",
+    ],
+  },
+  {
+    link: "https://karinkaaa.github.io/News/",
+    title: "News",
+    subtitle: "Integration with an external server",
+    technologies: [
+      "JavaScript",
+      "React",
+      "Redux",
+      "Redux-Saga",
+      "Material UI",
+      "HTML",
+      "CSS",
+    ],
+  },
+  {
+    link: "https://karinkaaa.github.io/portfolio/",
+    title: "Portfolio",
+    subtitle: "Development of my portfolio site",
+    technologies: [
+      "JavaScript",
+      "THREE.js",
+      "React",
+      "Material UI",
+      "HTML",
+      "CSS",
+    ],
+  },
+  {
+    link: "https://karinkaaa.github.io/Stamina/",
+    title: "Stamina",
+    subtitle: "Online program for mastering ten-finger typing and touch typing",
+    technologies: ["JavaScript", "React", "Material UI", "HTML", "CSS"],
+  },
+  {
+    link: "https://karinkaaa.github.io/spin-admin-deploy",
+    title: "Spin Admin",
+    subtitle: "Development of the admin part for the Spinner game",
+    technologies: [
+      "JavaScript",
+      "React",
+      "Redux",
+      "Redux-Saga",
+      "Mirage.js",
+      "Faker",
+      "Material UI",
+      "HTML",
+      "CSS",
+    ],
+  },
+  {
+    link: "https://karinkaaa.github.io/Products-front/",
+    title: "Products",
+    subtitle: "Home project for learning JavaScript, React and Redux",
+    technologies: [
+      "JavaScript",
+      "React",
+      "Redux",
+      "Redux-Saga",
+      "Material UI",
+      "HTML",
+      "CSS",
+    ],
+  },
 ];
 
 export default ProjectItems;
